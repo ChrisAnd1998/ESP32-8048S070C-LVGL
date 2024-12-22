a80d9f388ff0b3c8d6cce9cf49db38cfdc8575ef
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2024-12-22 13:00:52 +0100

    Update DisplaySetup.h

--- Example/DisplaySetup.h
+++ Example/DisplaySetup.h
@@ -1,11 +1,11 @@
 //ESP32-S3-BOX 8048S070C - 7 inch TFT-LCD Display 800*480 pixels with Touchscreen
-//Espressif 2.0.17
+//Espressif 3.0.7
 
 #ifndef DISPLAY_SETUP_H
 #define DISPLAY_SETUP_H
 
 #include <lvgl.h> /* 8.4.0 */
-#include <Arduino_GFX_Library.h> /* 1.3.1 */
+#include <Arduino_GFX_Library.h> /* 1.5.0 */
 #include "TAMC_GT911.h" /* 1.0.2 */
 
 // Configuration for Display and Touch
@@ -19,21 +19,16 @@
 #define TOUCH_MAP_Y1 480
 #define TOUCH_MAP_Y2 0
 
-// Global variables for display and touch
-Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-    GFX_NOT_DEFINED, GFX_NOT_DEFINED, GFX_NOT_DEFINED,
-    41, 40, 39, 42, 
-    14, 21, 47, 48, 45,
-    9, 46, 3, 8, 16, 1,
-    15, 7, 6, 5, 4
-);
-
-Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
-    bus, 
-    800, 0, 210, 30, 16,
-    480, 0, 22, 13, 10, 
-    1, 16000000, true
-);
+#define GFX_BL 2
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+    41 /* DE */, 40 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
+    14 /* R0 */, 21 /* R1 */, 47 /* R2 */, 48 /* R3 */, 45 /* R4 */,
+    9 /* G0 */, 46 /* G1 */, 3 /* G2 */, 8 /* G3 */, 16 /* G4 */, 1 /* G5 */,
+    15 /* B0 */, 7 /* B1 */, 6 /* B2 */, 5 /* B3 */, 4 /* B4 */,
+    0 /* hsync_polarity */, 180 /* hsync_front_porch */, 30 /* hsync_pulse_width */, 16 /* hsync_back_porch */,
+    0 /* vsync_polarity */, 12 /* vsync_front_porch */, 13 /* vsync_pulse_width */, 10 /* vsync_back_porch */);
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+    800 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */);
 
 TAMC_GT911 ts = TAMC_GT911(TOUCH_GT911_SDA, TOUCH_GT911_SCL, TOUCH_GT911_INT, TOUCH_GT911_RST, max(TOUCH_MAP_X1, TOUCH_MAP_X2), max(TOUCH_MAP_Y1, TOUCH_MAP_Y2));
 
